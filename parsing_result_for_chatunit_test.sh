3c1d538032ba82c41c3cea4511531d5d09605df3
Shanto Rahman <bit0321@iit.du.ac.bd>
2025-11-29 17:33:51 -0600

    bash comman

--- parsing_result_for_chatunit_test.sh
+++ parsing_result_for_chatunit_test.sh
@@ -1,4 +1,12 @@
-cp -r scripts/ /home/shanto/CHATUNITEST_backups_0.2_default
-cd /home/shanto/CHATUNITEST_backups_0.2_default
+#Command: bash parsing_result_for_chatunit_test.sh "CHATUNITEST_backups_0.2_SYMPROMPT"
+#$1="CHATUNITEST_backups_0.2_default"
+if [[ $1 == "" ]]; then
+  echo "give arg (e.g., CHATUNITEST_backups_0.2_default)"
+  exit
+fi
+cp -r scripts/ /home/shanto/$1
+#CHATUNITEST_backups_0.2_default
+cd /home/shanto/$1
+#CHATUNITEST_backups_0.2_default
 cp "quality_summary.csv" "quality_summary - Copy.csv"
 python3 scripts/annotate_csv_counts.py
